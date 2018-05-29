49024ac9ee1ee3f38f3945d9f6d465414f0e7b75
Daniel Peixoto <danielspeixoto@outlook.com>
2018-05-29 20:52:10 -0300

    dfs

--- src/test/ratings.js
+++ src/test/ratings.js
@@ -1,9 +1,30 @@
 const app = require('../main/app')
+const axios = require('axios')
+const Schedule = require('../main/data/Schedule')
+const Rating = require('../main/data/Rating')
+const User = require('../main/data/User')
+
+const port = 8080
 
 app.start({
     path: 'mongodb://localhost:27017/reclab',
     secret: 'secret'
-}, 8080)
+}, port)
+
+const schedule = new Schedule({
+    scheduleStart: 8,
+    ScheduleEnd: 9,
+    overallRating: 3
+})
+
+schedule.save(err => {
+    console.log(err)
+})
+
+axios.get('localhost:' + port + "/scheduling")
+    .then(resp => {
+        console.log(resp)
+    })
 
 
 
